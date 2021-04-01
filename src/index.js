49481fe898c231be629a27a0feffe2d029f60ecb
Matthew Mone <mmone@vernier.com>
2021-03-31 22:29:08 -0700

    more avatar

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ bot.on('ready', async () => {
     console.table({ BOTID: process.env.BOTID, TESTSERVER: process.env.TESTSERVER });
   }
   else {
-    bot.user.edit({ avatar: fs.readFile('./images/avatar.png') });
+    bot.user.edit({ avatar: fs.readFile('./src/images/avatar.png') });
     bot.user.setPresence({ activity: { name: 'casino games', type: 1 }, status: 'idle' });
   }
 });
